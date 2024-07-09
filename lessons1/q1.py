1c60040203fb6315b499fa88820da79025794f19
Gorienich <107270919+Gorienich@users.noreply.github.com>
2024-07-09 23:18:29 +0300

    Delete lessons1 directory

--- lessons1/q1.py
+++
@@ -1,7 +0,0 @@
-if (a > b and a < c) or (a < b and a > c):
-    print("beetwen bumber" + str(a))
-else:
-    if (b > a and b < c) or (b < a and b > c):
-        print("beetwen bumber" + str(b))
-    else:
-        print("beetwen bumber" + str(c))
